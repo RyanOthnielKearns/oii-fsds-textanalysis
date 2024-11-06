4c3a2ddc643084e3a55891e96c7b5c63598bf98b
Ryan Othniel Kearns <ryankearns25@gmail.com>
2024-11-06 19:00:50 +0000

    Add reddit username to setting; format analysis file

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
+import os
+
 # config/settings.py
 # Remember to replace the placeholder values with your own Reddit API credentials.
-USER_AGENT = "SDS_textanalysis/1.0 (by /u/yourusername)
+USER_AGENT = f"SDS_textanalysis/1.0 (by /u/{os.getenv('REDDIT_USERNAME')})"
 API_BASE_URL = "https://api.reddit.com"
 RATE_LIMIT_DELAY = 2
